8ee5c5df246de4393e8c1acd57b17933b520644d
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-10-30 18:46:19 +0530

    redirect

--- index.py
+++ index.py
@@ -1,11 +1,16 @@
 import web
 
 urls = ("/.*", "hello")
+urls = ('/(.*)/', 'redirect')
 app = web.application(urls, globals())
 
 class hello:
     def GET(self):
         return 'Hello, world!'
 
+class redirect:
+    def GET(self, path):
+        web.seeother('/' + path)
+        
 if __name__ == "__main__":
     app.run()
